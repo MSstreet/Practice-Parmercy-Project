62a0c2ac408f428fe33cb8d37b519c759f3c1127
MSstreet <cortm5pp@gmail.com>
2023-03-22 09:53:55 +0900

    first commit

---
+++ src/main/java/com/practice/parmercy/ParmercyApplication.java
@@ -0,0 +1,13 @@
+package com.practice.parmercy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParmercyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParmercyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/parmercy/ParmercyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.parmercy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParmercyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
